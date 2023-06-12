8c39f57b6f057cfec0ccd6b99cf3040a70d1aa12
adamkr98 <adamkrynski2@gmail.com>
2023-06-12 10:24:29 +0200

    I added li's to the ul after [i] square is clicked and its colorname as well as time delay is added

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
+
+let displayedSquare = document.createElement("div");
+displayedSquare.classList.add(".displayedSquare-wrapper");
+
+
+
 const _initTime = Date.now()
 
 const getElapsedTime = () => {
   return Number((Date.now() - _initTime) / 1000).toFixed(2) + 's'
 }
 
+
+let ul = document.querySelector("ul");
+
 const clickOnSquare = (e) => {
-  console.log(e.target.classList[1])
-  console.log(getElapsedTime())
-}
+    let colorName = e.target.classList[1];
+    let time = getElapsedTime();
+
+        let li = document.createElement("li");
+
+            li.innerHTML = "[" + time + "]" + " " + "Created a new " + e.target.classList[ 1 ] + " " + "square";
+            ul.appendChild(li);
+    };
+
 
 const actionSquares = document.querySelectorAll('.actionsquare')
 for (let actionSquare of actionSquares) {
-  actionSquare.addEventListener('click', clickOnSquare)
+  actionSquare.addEventListener('click', clickOnSquare);
+
 }
+
+
+    
+   
